7735f1952c6643318ab261a538796249ba10ec69
Ben Eills <ben@beneills.com>
2013-09-12 18:45:47 +0100

    debug linode feed generation

--- _plugins/update_rss.rb
+++ _plugins/update_rss.rb
@@ -29,7 +29,7 @@ rss = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
   <rss version=\"2.0\">
 
   <channel>
-    <title>#{repository_name}</title>
+    <title>#{repository_path}</title>
     <description>Git commits to #{repository_name}</description>
     <link>#{url}</link>
     <lastBuildDate>#{Time.now}</lastBuildDate>

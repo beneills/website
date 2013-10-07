de15f7a470449a8c7e29f605c572ae8a388c20d7
Ben Eills <ben@beneills.com>
2013-10-07 02:17:36 +0100

    estimated reading time

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // this script requires jQuery
 $(document).ready(function() {
-
+    var time = Math.ceil(($(".markdown-body").text().length/5)/200);
+    var units = time == 1 ? "min" : "mins";
+    $("#reading-time").text(time + " " + units);
 });
 
 // // Beeminder Image Fade
@@ -11,3 +13,5 @@ $(document).ready(function() {
 window.handleBeeminderImageLoad = function(obj) {
     $(obj).fadeIn(500);
 };
+
+

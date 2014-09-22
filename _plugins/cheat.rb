9742ba9105da25d102303b7de2647fd9dc2a6974
Ben Eills <ben@beneills.com>
2014-09-22 20:34:50 +0100

    Add cheat

---
+++ _plugins/cheat.rb
@@ -0,0 +1,46 @@
+module Jekyll
+  $cheat_sheets = begin
+                    puts "Found YAML cheat definitions."
+                    YAML.load_file("external/cheat/cheat.yaml")
+                  rescue
+                    puts "Warning: did not find YAML cheat definitions."
+                    {}
+                  end
+
+  class CheatSheetTag < Liquid::Tag
+    def render(context)
+      output = "<ul class=\"cheat-sheets\">"
+      $cheat_sheets.each do |text, link|
+        output << %{<li><a href="#{link}">#{text}</a></li>}
+      end
+      output << '</ul>'
+      output
+    end
+  end
+  Liquid::Template.register_tag('cheat_sheet', Jekyll::CheatSheetTag)
+
+
+  class RedirectPage < Page
+    def initialize(site, base, dir, data)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = "#{data['text']}.html"
+      self.data = data
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'redirect.html')
+    end
+  end
+
+  class CheatSheetRedirectGenerator < Generator
+    safe true
+
+    def generate(site) 
+      puts "Generating cheat redirect pages."
+      $cheat_sheets.each do |text, link|
+        site.pages << RedirectPage.new(site, site.source, "cheat", {'text' => text, 'link' => link})
+      end
+    end
+  end
+end
---
+++ pages/cheat.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: Cheat
+comments: false
+description: the best computer cheat sheets, <a href="https://github.com/beneills/cheat">help out</a>
+published: true
+---
+Add `http``://beneills.com/cheat/%s.html` as a keyword search for rapid access, e.g. `cheat go<RET>`.
+
+{% cheat_sheet %}
